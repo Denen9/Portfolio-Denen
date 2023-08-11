16ce93660e8e6fd6d6724705f64c2ad37eb3e47f
Denen <denenxz@gmail.com>
2023-08-11 07:35:21 -0300

    Cambios generales

--- Java Script/scroll-navbar.js
+++ Java Script/scroll-navbar.js
@@ -1,12 +1,17 @@
-const header = document.getElementById("header")
 
-  function changeNavbarBackground() {
-    if (window.scrollY > 0) {
-      navbarContainer.classList.add('solid-background');
+
+function changeNavbarBackground() {
+    const navbarContainer = document.getElementById("header");
+  
+    if (window.innerWidth > 1024) {
+      if (window.scrollY > 0) {
+        navbarContainer.classList.add('solid-background');
+      } else {
+        navbarContainer.classList.remove('solid-background');
+      }
     } else {
       navbarContainer.classList.remove('solid-background');
     }
   }
   
-
   window.addEventListener('scroll', changeNavbarBackground);
